d8b0ba89fe0b36b1be4b83639b3c47fe16bc98fc
Robert Griswold <robbob4@users.noreply.github.com>
2016-05-03 03:18:43 -0700

    Deconstructor pseudocode
    Bare bones for deconstructor inside the constructor script.

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -1,11 +1,13 @@
 ﻿// -------------------------------- Room.cs -----------------------------------
 // Author - Robert Griswold CSS 385
 // Created - May 2, 2016
-// Modified - May 2, 2016
+// Modified - May 3, 2016
 // ----------------------------------------------------------------------------
 // Purpose - Implementation for a genreic room class.
 // ----------------------------------------------------------------------------
-// Notes - Child must set _theRoom to load the prefab.
+// Notes - Child must set _theRoom to load the prefab. Temp can only be 
+// changed while it is true. Happiness values are only valid between 0-100.
+// CurrentCapacity values only valid between 0-MaxCapacity.
 // ----------------------------------------------------------------------------
 
 using UnityEngine;
@@ -26,9 +28,9 @@ public class Room : MonoBehaviour
     #region Variables
     //construction variables
     protected static int _constructionCost = 0; //0-x
-    protected static Size _roomSize = Size.Small; //this might need to be public
+    protected static Size _roomSize = Size.Small; //width of the room
     protected static string _prefabLocation = "";
-    private bool temp = true; //bool to flag this as a new room
+    private bool _temp = true; //bool to flag this as a new room
 
     //live variables
     protected int _spawnChance = 50; //0-100%
@@ -36,20 +38,35 @@ public class Room : MonoBehaviour
     protected int _maint = 0; //0-x
     protected int _happiness = 0; //0-100%
     protected int _capacity = 1; //0-x
+    private int _currentCapacity = 0; //0-_capacity
     protected Texture _tex = null;
     #endregion
 
-    // Use this for initialization
+    // Use this for fast initialization
     void Awake ()
     {
 	
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 	
 	}
+
+    #region Deconstruction
+    //announcment that the room is about to be destroyed
+    public virtual void Evict()
+    {
+
+    }
+    #endregion
 
     #region Getters and setters
     //construction
@@ -66,6 +83,18 @@ public class Room : MonoBehaviour
     public string PrefabLocation
     {
         get { return _prefabLocation; }
+    }
+
+    public bool Temp
+    {
+        get { return _temp; }
+        set
+        {
+            if (_temp == true) //can only be temp once
+                _temp = value;
+            else
+                Debug.Log(value + " is invalid temp value for " + this + ".");
+        }
     }
 
     //live
@@ -91,12 +120,26 @@ public class Room : MonoBehaviour
         {
             if (value >= 0 && value <= 100)
                 _happiness = value;
+            else
+                Debug.Log(value + " is invalid happiness value for " + this + ".");
         }
     }
 
-    public int Capacity
+    public int MaxCapacity
     {
         get { return _capacity; }
+    }
+
+    public int CurrentCapacity
+    {
+        get { return _currentCapacity; }
+        set
+        {
+            if (value >= 0 && value <= _capacity)
+                _currentCapacity = value;
+            else
+                Debug.Log(value + " is invalid currentCapacity value for " + this + ".");
+        }
     }
     #endregion
 }
--- Assets/Scripts/Room/Shop.cs
+++ Assets/Scripts/Room/Shop.cs
@@ -1,7 +1,7 @@
 ﻿// -------------------------------- Shop.cs -----------------------------------
 // Author - Robert Griswold CSS 385
 // Created - May 2, 2016
-// Modified - May 2, 2016
+// Modified - May 3, 2016
 // ----------------------------------------------------------------------------
 // Purpose - Implementation for a shop room that inherits from the room class.
 // ----------------------------------------------------------------------------
@@ -13,16 +13,30 @@ using System.Collections;
 
 public class Shop : Room
 {
-	// Use this for initialization
+	// Use this for fast initialization
 	void Awake ()
     {
         _roomSize = Room.Size.Medium;
         _prefabLocation = "Prefabs/Room/Floor"; //TODO: Update this prefab
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Use this for initialization
+    void Start()
     {
-	    
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region Deconstruction
+    //announcment that the room is about to be destroyed
+    public override void Evict()
+    {
+
+    }
+    #endregion
 }
